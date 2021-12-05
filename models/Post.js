0c74463a751d8e56a12acfdab9ea357beaad20c2
paperpatch <patchen21@gmail.com>
2021-12-04 23:01:35 -0500

    added PUT route for voting

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { Vote } = require('.');
 const sequelize = require('../config/connection');
 // create our Post model
 class Post extends Model {}
