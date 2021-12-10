bee1bb04f6658fc0e37a5316674dba162bdec35d
paperpatch <patchen21@gmail.com>
2021-12-10 15:09:53 -0500

    fixed npm for session on back end

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -17,6 +30,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
