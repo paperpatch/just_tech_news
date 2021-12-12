4bdc95ace710fac9b97c1d57cc76ee6f3faf0ce1
paperpatch <patchen21@gmail.com>
2021-12-12 15:26:04 -0500

    14.5

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 const withAuth = require('../utils/auth');
 
+// get all posts for dashboard
 router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
   Post.findAll({
     where: {
-      // use the ID from the session
       user_id: req.session.user_id
     },
     attributes: [
@@ -32,7 +34,6 @@ router.get('/', withAuth, (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // serialize data before passing to template
       const posts = dbPostData.map(post => post.get({ plain: true }));
       res.render('dashboard', { posts, loggedIn: true });
     })
@@ -43,10 +44,7 @@ router.get('/', withAuth, (req, res) => {
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
+  Post.findByPk(req.params.id, {
     attributes: [
       'id',
       'post_url',
@@ -70,20 +68,18 @@ router.get('/edit/:id', withAuth, (req, res) => {
     ]
   })
     .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
       }
-
-      const post = dbPostData.get({ plain: true });
-
-      res.render('edit-post', {
-        post,
-        loggedIn: true
-      });
     })
     .catch(err => {
-      console.log(err);
       res.status(500).json(err);
     });
 });
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,27 +1,25 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-    const post_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length -1
-    ];
-
-    if (post_id) {
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          title
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-
-      if (response.ok){
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
